8fcef87127e20230b149c87679c2283af8039482
PashaKrasnoporov <61505136+PashaKrasnoporov@users.noreply.github.com>
2023-12-03 22:44:35 +0200

    Add files via upload

---
+++ Lab5/WebApp/Classes/Class1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using WebApp.Models;
+
+public class PermutationsGenerator
+{
+    private static void Swap(ref char a, ref char b)
+    {
+        char temp = a;
+        a = b;
+        b = temp;
+    }
+
+    private static void GeneratePermutations(char[] chars, int start, int end, HashSet<string> permutations)
+    {
+        if (start == end)
+        {
+            permutations.Add(new string(chars));
+            return;
+        }
+
+        for (int i = start; i <= end; i++)
+        {
+            Swap(ref chars[start], ref chars[i]);
+            GeneratePermutations(chars, start + 1, end, permutations);
+            Swap(ref chars[start], ref chars[i]);
+        }
+    }
+
+    public static HashSet<string> GeneratePermutations(string input)
+    {
+        char[] chars = input.ToCharArray();
+        HashSet<string> permutations = new HashSet<string>();
+        GeneratePermutations(chars, 0, chars.Length - 1, permutations);
+        return permutations;
+    }
+
+    public static void WritePermutationsToFile(HashSet<string> permutations, string filePath)
+    {
+        using (StreamWriter writer = new StreamWriter(filePath))
+        {
+            foreach (string permutation in permutations)
+            {
+                writer.WriteLine(permutation);
+            }
+        }
+    }
+}
+
+class Class1
+{
+    static void Main(string[] args)
+    {
+
+        //Function function1 = new Function();
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.InputEncoding = Encoding.UTF8;
+
+        string inputString = string.Empty;
+
+        while (true)
+        {
+            Console.WriteLine("Виберіть джерело даних:");
+            Console.WriteLine("1. Ввести рядок із консолі");
+            Console.WriteLine("2. Використати дані із файлу INPUT.TXT");
+            Console.Write("Введіть цифру для вибору: ");
+            string choice = Console.ReadLine();
+
+            if (choice == "1")
+            {
+                Console.WriteLine("Введіть рядок:");
+                inputString = Console.ReadLine().Trim();
+                break;
+            }
+            else if (choice == "2")
+            {
+                try
+                {
+                    inputString = File.ReadAllText("INPUT.TXT").Trim();
+                    break;
+                }
+                catch (FileNotFoundException)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Файл INPUT.TXT не знайдений.");
+                    Console.ResetColor();
+                }
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Невірний вибір. Будь ласка, виберіть правильну опцію.");
+                Console.ResetColor();
+            }
+        }
+
+        HashSet<string> permutations = PermutationsGenerator.GeneratePermutations(inputString);
+        PermutationsGenerator.WritePermutationsToFile(permutations, "OUTPUT.TXT");
+        //model1
+
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("Перестановки були записані в файл OUTPUT.TXT.");
+        Console.ResetColor();
+
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("Перестановки із файлу OUTPUT.TXT:");
+        string[] lines = File.ReadAllLines("OUTPUT.TXT");
+        foreach (string line in lines)
+        {
+            Console.WriteLine(line);
+        }
+        Console.ResetColor();
+    }
+}
---
+++ Lab5/WebApp/Controllers/FunctionsController.cs
@@ -0,0 +1,216 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.Numerics;
+using WebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+public class FunctionsController : Controller
+{
+    public IActionResult Function1()
+    {
+
+        var model = new Function
+        {
+            Name = "Генерація перестановок",
+            Description = $"Генерує всі можливі перестановки символів введеного рядка. Введіть рядок:",
+        // Other data
+        };
+        return View(model);
+    }
+
+    public async Task<IActionResult> GeneratePermutations([Bind("Input")] Function model)
+    {
+        try
+        {
+            // Simulate asynchronous processing (replace with actual asynchronous logic)
+            //await Task.Delay(1000);
+
+            var permutations = PermutationsGenerator.GeneratePermutations(model.Input);
+            model.Output = permutations;
+            // Other data
+
+            return View("Function1", model);
+        }
+        catch (Exception ex)
+        {
+            model.ErrorMessage = $"Помилка: {ex.Message}";
+            // Other data
+
+            return View("Function1", model);
+        }
+    }
+
+
+    public IActionResult Function2()
+    {
+        var model1 = new Function
+        {
+            Name = "Генерація послідовностей",
+            Description = "Ця функція реалізує обчислення та відображення кількості допустимих послідовностей для заданого числа N. Підтримує введення з консолі та зчитування з файлу INPUT.TXT.",
+            // Інші дані
+        };
+        return View(model1);
+    }
+
+    public async Task<IActionResult> GenerateSequences([Bind("Input")] Function model1)
+    {
+        try
+        {
+            // Simulate asynchronous processing (replace with actual asynchronous logic)
+            //await Task.Delay(1000);
+
+            int inputValue = int.Parse(model1.Input);
+            BigInteger permutations = Class2.CountValidSequences(inputValue);
+            model1.Output = new HashSet<string> { permutations.ToString() };
+
+            // Other data
+
+            return View("Function2", model1);
+        }
+        catch (Exception ex)
+        {
+            model1.ErrorMessage = $"Помилка: {ex.Message}";
+            // Other data
+
+            return View("Function2", model1);
+        }
+    }
+
+    public IActionResult Function3()
+    {
+        var model3 = new Puzzle
+        {
+            Name = "Вирішити Головоломку",
+            Description = "Метод, який вирішує головоломку з мінімізацією перестановок.",
+            // Other data
+        };
+        return View(model3);
+    }
+
+    public async Task<IActionResult> SolvePuzzle([Bind("Input, EndInput, NumInput")] Puzzle model3)
+    {
+        try
+        {
+            // Simulate asynchronous processing (replace with actual asynchronous logic)
+            //await Task.Delay(1000);
+
+            char[] startValue = (model3.Input).ToCharArray();
+            char[] endValue = (model3.EndInput).ToCharArray();
+
+           
+            int rows = model3.NumInput;
+            int cols = startValue.Length / rows;
+            char[,] start = new char[rows, cols];
+            char[,] end = new char[rows, cols];
+
+
+            for (int i = 0; i < rows; i++)
+            {
+                for(int j = 0; j < cols; j++)
+                {
+                    end[i, j] = endValue[i * cols + j];
+                }
+            }
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    start[i, j] = startValue[i * cols + j];
+                }
+            }
+
+            int result = MinimumMoves(start, end);
+            model3.Output = new HashSet<string> { result.ToString() }; ;
+            
+
+
+            return View("Function3", model3);
+        }
+        catch (Exception ex)
+        {
+            model3.ErrorMessage = $"Помилка: {ex.Message}";
+            // Other data
+
+            return View("Function3", model3);
+        }
+    }
+
+    [Authorize]
+    public IActionResult FunctionsList(Function model)
+    {
+        return View();
+    }
+
+    public int MinimumMoves(char[,] startState, char[,] targetState)
+    {
+        Dictionary<string, int> distances = new Dictionary<string, int>();
+        Queue<string> queue = new Queue<string>();
+        queue.Enqueue(StateToString(startState));
+        distances[StateToString(startState)] = 0;
+
+        while (queue.Count > 0)
+        {
+            string currentState = queue.Dequeue();
+
+            if (currentState == StateToString(targetState))
+                return distances[currentState];
+
+            int emptyIndex = currentState.IndexOf('#');
+
+            int[] dx = { -1, 1, 0, 0 };
+            int[] dy = { 0, 0, -1, 1 };
+
+            for (int i = 0; i < 4; i++)
+            {
+                int newRow = emptyIndex / 4 + dx[i];
+                int newCol = emptyIndex % 4 + dy[i];
+
+                if (IsValidMove(newRow, newCol))
+                {
+                    string nextState = SwapCharacters(currentState, emptyIndex, newRow * 4 + newCol);
+                    if (!distances.ContainsKey(nextState))
+                    {
+                        distances[nextState] = distances[currentState] + 1;
+                        queue.Enqueue(nextState);
+                    }
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    public bool IsValidMove(int row, int col)
+    {
+        return row >= 0 && row < 2 && col >= 0 && col < 4;
+    }
+
+    public string StateToString(char[,] state)
+    {
+        string result = "";
+        for (int i = 0; i < 2; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                result += state[i, j];
+            }
+        }
+        return result;
+    }
+
+    public string SwapCharacters(string str, int index1, int index2)
+    {
+        char[] characters = str.ToCharArray();
+        char temp = characters[index1];
+        characters[index1] = characters[index2];
+        characters[index2] = temp;
+        return new string(characters);
+    }
+}
+
+    // Repeat for other functions
