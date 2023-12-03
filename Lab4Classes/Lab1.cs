1c86ffa992fe20c5d74d458fd101994a2662a4aa
PashaKrasnoporov <61505136+PashaKrasnoporov@users.noreply.github.com>
2023-12-03 22:05:54 +0200

    Add files via upload

---
+++ Lab4Classes/Lab1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Lab4Classes
+{
+    public class Lab1
+    {
+        // Метод для обміну значень двох символів
+        private static void Swap(ref char a, ref char b)
+        {
+            char temp = a;
+            a = b;
+            b = temp;
+        }
+
+        // Рекурсивна функція для генерації перестановок
+        private static void GeneratePermutations(char[] chars, int start, int end, HashSet<string> permutations)
+        {
+            // Якщо досягли кінця масиву, додаємо поточну перестановку в множину
+            if (start == end)
+            {
+                permutations.Add(new string(chars));
+                return;
+            }
+
+            // Генерація перестановок за допомогою рекурсії
+            for (int i = start; i <= end; i++)
+            {
+                Swap(ref chars[start], ref chars[i]);
+                GeneratePermutations(chars, start + 1, end, permutations);
+                Swap(ref chars[start], ref chars[i]);
+            }
+        }
+
+        // Метод для генерації перестановок з рядка input
+        public static HashSet<string> GeneratePermutations(string input)
+        {
+            char[] chars = input.ToCharArray();
+            HashSet<string> permutations = new HashSet<string>();
+            GeneratePermutations(chars, 0, chars.Length - 1, permutations);
+            return permutations;
+        }
+
+        // Метод для запису перестановок у файл
+        public static void WritePermutationsToFile(HashSet<string> permutations, string filePath)
+        {
+            // Використання блоку using для автоматичного закриття файлового потоку
+            using (StreamWriter writer = new StreamWriter(filePath))
+            {
+                foreach (string permutation in permutations)
+                {
+                    writer.WriteLine(permutation);
+                }
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            // Налаштування кодування для правильного відображення символів (UTF-8)
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.InputEncoding = Encoding.UTF8;
+
+            string inputString = string.Empty;
+
+            // Безкінечний цикл для вибору джерела даних
+            while (true)
+            {
+                Console.WriteLine("Виберіть джерело даних:");
+                Console.WriteLine("1. Ввести рядок із консолі");
+                Console.WriteLine("2. Використати дані із файлу INPUT.TXT");
+                Console.Write("Введіть цифру для вибору: ");
+                string choice = Console.ReadLine();
+
+                if (choice == "1")
+                {
+                    Console.WriteLine("Введіть рядок:");
+                    inputString = Console.ReadLine().Trim();
+                    break; // Завершуємо цикл, якщо вибрано 1
+                }
+                else if (choice == "2")
+                {
+                    try
+                    {
+                        // Зчитуємо дані з файлу INPUT.TXT
+                        string[] lines = File.ReadAllLines("INPUT.TXT");
+                        inputString = string.Join(Environment.NewLine, lines).Trim();
+                        break; // Завершуємо цикл, якщо вибрано 2
+                    }
+                    catch (FileNotFoundException)
+                    {
+                        // Обробка винятку, якщо файл не знайдено
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Файл INPUT.TXT не знайдений.");
+                        Console.ResetColor();
+                    }
+                }
+                else
+                {
+                    // Повідомлення про невірний вибір
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Невірний вибір. Будь ласка, виберіть правильну опцію.");
+                    Console.ResetColor();
+                }
+            }
+
+            // Генерація перестановок та запис у файл
+            HashSet<string> permutations = GeneratePermutations(inputString);
+            WritePermutationsToFile(permutations, "OUTPUT.TXT");
+
+            // Виведення повідомлення про успішне завершення та перегляд результатів
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Перестановки були записані в файл OUTPUT.TXT.");
+            Console.ResetColor();
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Перестановки із файлу OUTPUT.TXT:");
+            string[] lines = File.ReadAllLines("OUTPUT.TXT");
+            foreach (string line in lines)
+            {
+                Console.WriteLine(line);
+            }
+            Console.ResetColor();
+        }
+    }
+}
---
+++ Lab4Classes/Lab3.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Lab4Classes
+{
+    public static class Lab3
+    {
+        static int[] dr = { -1, 0, 1, 0 };
+        static int[] dc = { 0, 1, 0, -1 };
+        static char[] keys = { 'R', 'G', 'B', 'Y' };
+        static int[] keyCosts;
+
+        public static void RunLab(string inputFile, string outputFile)
+        {
+            using (StreamReader sr = new StreamReader(inputFile))
+            using (StreamWriter sw = new StreamWriter(outputFile))
+            {
+                try
+                {
+                    var dimensions = ReadArray<int>(sr, 2);
+                    int R = dimensions[0];
+                    int C = dimensions[1];
+
+                    keyCosts = ReadArray<int>(sr, keys.Length);
+
+                    char[][] maze = new char[R][];
+                    for (int i = 0; i < R; i++)
+                    {
+                        maze[i] = sr.ReadLine()?.ToCharArray() ?? throw new InvalidDataException("Incorrect input data in the maze.");
+                    }
+
+                    int result = BFS(maze, R, C);
+                    if (result == -1)
+                    {
+                        sw.WriteLine("Sleep");
+                    }
+                    else
+                    {
+                        sw.WriteLine(result);
+                    }
+                }
+
+                catch (Exception ex)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"Error: {ex.Message}");
+                    Console.ResetColor();
+                    Console.WriteLine("Press any key to exit...");
+                    Console.ReadKey();
+                }
+            }
+        }
+
+        static T[] ReadArray<T>(StreamReader sr, int length)
+        {
+            var parts = sr.ReadLine()?.Split();
+            if (parts == null || parts.Length != length)
+            {
+                throw new InvalidDataException("Incorrect input data format.");
+            }
+
+            var result = new T[length];
+            for (int i = 0; i < length; i++)
+            {
+                if (!TryParse(parts[i], out result[i]))
+                {
+                    throw new InvalidDataException("Could not convert string to number.");
+                }
+            }
+
+            return result;
+        }
+
+        static bool TryParse<T>(string s, out T result)
+        {
+            try
+            {
+                result = (T)Convert.ChangeType(s, typeof(T));
+                return true;
+            }
+            catch
+            {
+                result = default;
+                return false;
+            }
+        }
+
+        static int BFS(char[][] maze, int R, int C)
+        {
+            Queue<(int, int, int, int)> queue = new Queue<(int, int, int, int)>();
+            bool[,,] visited = new bool[R, C, 16];
+            for (int i = 0; i < R; i++)
+            {
+                for (int j = 0; j < C; j++)
+                {
+                    if (maze[i][j] == 'S')
+                    {
+                        queue.Enqueue((i, j, 0, 0));
+                        visited[i, j, 0] = true;
+                    }
+                }
+            }
+
+            while (queue.Count > 0)
+            {
+                var (r, c, state, cost) = queue.Dequeue();
+                if (maze[r][c] == 'E') return cost;
+
+                for (int d = 0; d < 4; d++)
+                {
+                    int nr = r + dr[d];
+                    int nc = c + dc[d];
+                    int nstate = state;
+                    int ncost = cost;
+
+                    if (nr >= 0 && nr < R && nc >= 0 && nc < C && maze[nr][nc] != 'X' && !visited[nr, nc, state])
+                    {
+                        if (Array.IndexOf(keys, maze[nr][nc]) != -1)
+                        {
+                            int keyIndex = Array.IndexOf(keys, maze[nr][nc]);
+                            if ((state & (1 << keyIndex)) == 0)
+                            {
+                                nstate |= (1 << keyIndex);
+                                ncost += keyCosts[keyIndex];
+                            }
+                        }
+
+                        visited[nr, nc, nstate] = true;
+                        queue.Enqueue((nr, nc, nstate, ncost));
+                    }
+                }
+            }
+
+            return -1;
+        }
+    }
+}
