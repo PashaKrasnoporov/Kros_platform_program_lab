497b13ae0c556dbc53f069e1e307e42ce8990cb9
PashaKrasnoporov <61505136+PashaKrasnoporov@users.noreply.github.com>
2023-12-07 22:59:08 +0200

    Delete lab13/frontend directory

--- lab13/frontend/src/components/index.ts
+++
@@ -1,5 +0,0 @@
-export { Home } from './Home';
-export { Navbar } from './Navbar';
-export { Lab1 } from './Lab1';
-export { Lab2 } from './Lab2';
-export { Lab3 } from './Lab3';
--- lab13/frontend/src/config/index.ts
+++
@@ -1,7 +0,0 @@
-export default {
-  Auth0: {
-    domain: 'dev-u8hg7vily1uln7eo.us.auth0.com',
-    clientId: 'DIen319xhTzsqYfZ4AMYg5pkne5Fg4U0',
-    audience: 'https://127.0.0.1:7035'
-  }
-}
